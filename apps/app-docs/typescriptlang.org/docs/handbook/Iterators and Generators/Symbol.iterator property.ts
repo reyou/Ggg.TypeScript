7b76f7dba162a1b543e458050c48296f3542eef8
Teoman Shipahi <aytekoz@yahoo.com>
2018-10-28 11:36:01 -0400

    pre tsconfig change

--- apps/app-docs/typescriptlang.org/docs/handbook/Iterators and Generators/Symbol.iterator property.ts
+++ apps/app-docs/typescriptlang.org/docs/handbook/Iterators and Generators/Symbol.iterator property.ts
@@ -1,5 +1,6 @@
 /*
 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+* https://stackoverflow.com/questions/53028291/element-implicitly-has-an-any-type-because-type-setstring-has-no-index-sig
 */
 /*Another distinction is that for..in operates on any object; 
 it serves as a way to inspect properties on this object. for..of on 
@@ -7,8 +8,14 @@ the other hand, is mainly interested in values of iterable objects.
 Built-in objects like Map and Set implement Symbol.iterator property 
 allowing access to stored values. */
 function SymbolIterator() {
-  let pets = new Set(["Cat", "Dog", "Hamster"]);
-  //pets["species"] = "mammals";
+  // let pets = new Set(["Cat", "Dog", "Hamster"]);
+  let pets: Set<string> & { species?: string } = new Set([
+    "Cat",
+    "Dog",
+    "Hamster"
+  ]);
+
+  pets["species"] = "mammals";
 
   for (let pet in pets) {
     console.log(pet); // "species"
