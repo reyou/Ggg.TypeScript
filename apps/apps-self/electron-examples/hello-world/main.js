3247bc8cef0db4ecd3cf049292877d50ae9bd78b
Aytekin Ozdemir <aytekoz@yahoo.com>
2021-04-06 23:06:05 -0500

    electron hello world

---
+++ apps/apps-self/electron-examples/hello-world/main.js
@@ -0,0 +1,7 @@
+console.log('main process working');
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
