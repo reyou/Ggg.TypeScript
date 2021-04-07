b915814239c0e360a934afd0b9ea2169644f3828
aytekin ozdemir <aytekin.ozdemir@blackbaud.me>
2021-04-07 11:49:29 -0500

    Electron js Tutorial - 2 - Hello World

--- apps/apps-self/electron-examples/hello-world/main.js
+++ apps/apps-self/electron-examples/hello-world/main.js
@@ -3,5 +3,45 @@ console.log('main process working');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const path = require('path');
-const url = require('url');
+
+let win;
+
+function createWindow() {
+  // https://www.electronjs.org/docs/api/browser-window
+  win = new BrowserWindow();
+
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  win.on('closed', () => {
+    console.log({
+      title: 'window-on-closed'
+    });
+    // this can be garbage collected
+    win = null;
+  });
+}
+
+app.on('ready', () => {
+  console.log({
+    title: 'app-on-ready'
+  });
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  console.log({
+    title: 'window-all-closed'
+  });
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  console.log({
+    title: 'app-on-activate'
+  });
+  if (win == null) {
+    createWindow();
+  }
+});
